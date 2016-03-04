8a89b655f35077f19a01406c11af4f43f68fe341
satish935 <satish935@gmail.com>
2016-03-05 01:19:40 +0200

    Update and rename WritingSeleniumCode.java to NordeaAssignment.java

--- src/practice/com/NordeaAssignment.java
+++ src/practice/com/NordeaAssignment.java
@@ -7,16 +7,29 @@ import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 
-public class WritingSeleniumCode {
+public class NordeaAssignment {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+//Create Firefox driver to drive browser
 WebDriver driver=new FirefoxDriver(new FirefoxProfile());
 driver.manage().window().maximize();
+
+//Open Amazon home page 
 driver.get("http://www.amazon.com");
+
+//Enter Nikon in search field and submit
 driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Nikon");
 driver.findElement(By.className("nav-input")).click();
+try {
+	Thread.sleep(2000);
+} catch (InterruptedException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
 
+// Select Sort by dropdown and then select High to Low option
 Select dropdown = new Select(driver.findElement(By.id("sort")));
 dropdown.selectByIndex(3);
 try {
@@ -25,8 +38,14 @@ try {
 	// TODO Auto-generated catch block
 	e.printStackTrace();
 }
+
+//Click Nikon D3X image
 driver.findElement(By.cssSelector("#result_0 > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1)")).click();
+
+// Verify Nikon D3X text on the product title
 Assert.assertTrue(driver.findElement(By.id("productTitle")).getText().contains("Nikon D3X"));
+
+//close browser
 driver.quit();
 	}
 }
